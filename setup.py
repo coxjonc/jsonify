4f57d3b823a8457097bb2ccaaf3cf6bbffb79260
geezhawk <jonathan.cox.c@gmail.com>
2016-04-29 14:54:07 +0700

    first commit

---
+++ setup.py
@@ -0,0 +1,17 @@
+"""
+A tool for quickly converting .csv files 
+to .json format
+"""
+
+from distutils.core import setup
+
+setup(
+    name='jsonify',
+    version='0.5',
+    packages=['jsonify',],
+    description='A csv to json converter',
+    author='Jonathan Cox',
+    author_email='jonathan.cox.c@gmail.com',
+    license='MIT',
+    keywords='csv json'
+)
